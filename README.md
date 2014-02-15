dec8cda6fe0489c2ffe77e09eecc2b0458a30436
Josef Ottosson <jo222ev@student.lnu.se>
2014-02-15 13:11:33 +0100

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,53 @@ rest-proxy
 ==========
 
 Simple PHP Rest-proxy, supports GET, POST, PUT and DELETE
+
+Example Requests:
+
+POST
+```
+var player = {};
+player.Name = 'Zlatan Ibrahimovic';
+
+$.ajax({
+   url: 'index.php?url=http://www.arsenal.com/addPlayer/',
+   type: 'POST',
+   data: { data: JSON.stringify(player) },
+   success: function(data) {
+     console.log(data);
+   }
+});
+```
+
+GET
+```
+$.get( "index.php?url=http://www.arsenal.com", function( data ) {
+  console.log(data);
+});
+```
+
+PUT
+```
+var player = {};
+player.Goals = 10000;
+
+$.ajax({
+   url: 'index.php?url=http://www.arsenal.com/players/10',
+   type: 'PUT',
+   data: JSON.stringify(player),
+   success: function(data) {
+     console.log(data);
+   }
+});
+```
+
+DELETE
+```
+$.ajax({
+    url: 'index.php?url=http://www.manutd.com/players/10',
+    type: 'DELETE',
+    success: function(data) {
+        console.log(data);
+    }
+});
+```
---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+
+  $url = $_REQUEST["url"];
+
+  if(!$url) {
+    echo "You need to pass in a target URL.";
+    return;
+  }
+
+  $response = "";
+  switch (getMethod()) {
+    case 'POST':
+      $response = makePostRequest(getPostData(), $url);
+      break;
+    case 'PUT':
+      $response = makePutRequest(getPutOrDeleteData($url), $url);
+      break;
+    case 'DELETE':
+      $response = makeDeleteRequest($url);
+      break;
+    case 'GET':
+      $response = makeGetRequest($url);
+      break;
+    default:
+      echo "This proxy only supports POST, PUT, DELETE AND GET REQUESTS.";
+      return;
+  }
+
+  echo $response;
+
+  function getMethod() {
+    return $_SERVER["REQUEST_METHOD"]; 
+  }
+
+  function getPostData() {
+    return http_build_query($_POST);
+  }
+
+  function getPutOrDeleteData($url) {
+    $data = substr(file_get_contents('php://input'), strlen($url));
+    return $data;
+  }
+
+  function makePostRequest($data, $url) {
+    $httpHeader = array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data));
+    
+    return makePutOrPostCurl('POST', $data, true, $httpHeader, $url);
+  }
+
+  function makePutRequest($data, $url) {
+
+    return makePutOrPostCurl('PUT', $data, true, $httpHeader, $url);
+  }
+
+  function makeDeleteRequest($url) {
+    $ch = initCurl($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+  }
+
+  function makeGetRequest($url) {
+    $ch = initCurl($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+  }
+
+  function makePutOrPostCurl($type, $data, $returnTransfer, $httpHeader, $url) {
+
+    $ch = initCurl($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, $returnTransfer);
+    
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+  }
+
+  function initCurl($url) {
+    $httpHeader = array(
+    'Content-Type: application/x-www-form-urlencoded');
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $httpHeader);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36');
+
+    return $ch;
+  }
+
+
+?>
